a82a79479bfce2c57e262d1d00546ab916c1e929
razken-hash <ka_kenniche@esi.dz>
2024-04-29 11:17:09 +0100

    Parkir_API Init

---
+++ src/test/java/com/parkir/parkir_api/ParkirApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parkir.parkir_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkirApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
